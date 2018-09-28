3f5c5ffea63c274702548ee903af1fa1f5e4fef5
ZsmallMonster <2745872565@qq.com>
2018-09-28 16:36:44 +0800

    接收后台传来的json

--- yh-model/src/main/java/com/zhh/yh/controller/SysUserController.java
+++ yh-model/src/main/java/com/zhh/yh/controller/SysUserController.java
@@ -1,5 +1,8 @@
 package com.zhh.yh.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.zhh.yh.entity.SysUserEntity;
 import com.zhh.yh.service.SysUserService;
 
@@ -38,15 +43,19 @@ public class SysUserController {
 	
 	//@ModelAttribute
 	@RequestMapping("add")
-	public String saveUser(HttpServletRequest request,
+	public Map saveUser(HttpServletRequest request,
 			HttpServletResponse response,
 			SysUserEntity user) {
 		System.out.println("=======>>>>终于进来了！");
 		//user.setUsername("王刚");
 		//user.setPassword("123456");
 		sysUserService.saveUser(user);
+		Map<String, Object> result = new HashMap<String,Object>();
+		result.put("success", true);
+		result.put("msg", "保存成功");
+		JSON.toJSON(result);
 		//返回到用户列表用页面
-		return "success.html";//执行了，但是怎么就是不到这个页面。html页面里的ajax中的success也并未出现，
+		return result;//执行了，但是怎么就是不到这个页面。html页面里的ajax中的success也并未出现，
 	}
 	
 }
